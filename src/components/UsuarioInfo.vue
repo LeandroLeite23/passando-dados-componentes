f97dc015adad981233f62cdbb48c2cdb120130cd
lleite23 <leandrohenrique@frwk.com.br>
2023-03-22 15:16:43 -0300

    first commit

---
+++ src/components/UsuarioInfo.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong> {{ inverterLetras() }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="reinciarFn">Reiniciar Nome (CallBack)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            //required: true
+            default: 'Anônimo'
+            /* default: function() {
+                return Array(10).fill(23).join(',')
+            } */
+        },
+        reinciarFn: Function
+    },
+    methods: {
+        inverterLetras() {
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            /* eslint-disable */
+            const antigo = this.nome
+            
+            this.nome = 'Leandro'
+            //Evento para informar o pai que houve alteração
+            this.$emit('AlteradoPeloFilho', this.nome)
+            /* {
+                novo: this.nome,
+                antigo
+            }) */
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
